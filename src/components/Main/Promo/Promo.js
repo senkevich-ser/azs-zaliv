a940cc95034d80e49e74eccac24917860b6edf1e
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-11-15 18:07:07 +0400

    added background:cover

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -5,6 +5,7 @@ function Promo(props) {
   return (
     <section className="promo">
       {props.children}
+      <div className='promo__image'></div>
        <h1 className="promo__module promo__title">
           Создаем<br></br> устойчивое <br></br>энергетическое <br></br>будущее.
         </h1>
