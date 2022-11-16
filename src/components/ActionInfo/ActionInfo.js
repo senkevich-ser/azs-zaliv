5cf4443418ab72ce3ef921d2ed0d6a52a808516a
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-11-16 13:14:43 +0400

    added home button in ActionInfo

--- src/components/ActionInfo/ActionInfo.js
+++ src/components/ActionInfo/ActionInfo.js
@@ -1,18 +1,20 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
 import Header from '../Header/Header';
 import NavTab from '../Main/NavTab/NavTab';
 import action from '../../images/frankl.pdf';
+import { Link } from 'react-router-dom'
 import './ActionInfo.css';
 
 function ActionInfo() {
   return (
     <>
-    <Header>{<NavTab />}</Header>
-    <section className="actionInfo">
-      <h3 className='actionInfo__title'>Программа лояльности "Залив-бонус"</h3>
-      <iframe src={action}  className="actionInfo__text"></iframe>
-
-    </section >
+      <Header>{<NavTab />}</Header>
+      <section className="actionInfo">
+        <h3 className='actionInfo__title'>Программа лояльности "Залив-бонус"</h3>
+        <iframe title="" src={action} className="actionInfo__button"></iframe>
+        <Link to="/"><button className="actionInfo__button">Домой</button></Link>
+      </section >
     </>
   );
 }
