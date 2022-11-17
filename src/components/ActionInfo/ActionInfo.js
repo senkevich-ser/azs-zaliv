d127476ad6aa914d57d9429c6962d50343e08b55
Sergey Senkevich <accc75-mai.ru@yandex.ru>
2022-11-17 06:10:35 +0400

    remove general button

--- src/components/ActionInfo/ActionInfo.js
+++ src/components/ActionInfo/ActionInfo.js
@@ -12,8 +12,8 @@ function ActionInfo() {
       <Header>{<NavTab />}</Header>
       <section className="actionInfo">
         <h3 className='actionInfo__title'>Программа лояльности "Залив-бонус"</h3>
-        <iframe title="" src={action} className="actionInfo__button"></iframe>
-        <Link to="/"><button className="actionInfo__button">Домой</button></Link>
+        {/* <iframe title="" src={action} className="actionInfo__button"></iframe> */}
+        <Link to="/"><button className="actionInfo__button">Главная</button></Link>
       </section >
     </>
   );
