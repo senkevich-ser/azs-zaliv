4b4342c5a5edda3ea02febaf24fd48a78449b237
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-11-22 15:40:15 +0400

    change deploy path to root@62.217.178.177:/var/www/html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root"),
 );
